9cac729b1bcc0c3242d3373e0c65b98b72da2c13
Sheza Shamsher <160360815+ShezaShamsher@users.noreply.github.com>
2024-09-24 02:38:42 +0500

    Add files via upload
    Components

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header"
+import About from "@/components/About"
+
+function Home() {
+   return (
+  <>
+  <Header />
+  <About />
+  </>
+   )
+}
+
+export default Home
---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+
+    return (
+   <>
+
+   <h1 className="bg-purple-400" >Welcome to my website</h1>
+   </>
+
+    )
+}
+
+export default Header
